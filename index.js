4176415ab2fe4b43d3e092f879d13411d226214f
efyguhijok <bsjcigchrissy22@hotmail.com>
2024-05-15 01:16:49 +0700

    test task

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-function findItinerary(tickets) {
-  const graph = new Map();
-  for (const [from, to] of tickets) {
-    if (!graph.has(from)) graph.set(from, []);
-    graph.get(from).push(to);
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
   }
-  for (const destinations of graph.values()) {
-    destinations.sort();
-  }
-  const result = [];
-  dfs("JFK");
-  return result.reverse();
-  function dfs(from) {
-    const destinations = graph.get(from);
-    while (destinations && destinations.length) {
-      dfs(destinations.shift());
-    }
-    result.push(from);
-  }
+  return prev;
 }
